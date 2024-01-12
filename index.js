4c6b65ea785530553498e1ea2afd786f0ae10b22
Vasyl Popyk <vasyl.popyk2810@gmail.com>
2024-01-12 15:22:49 +0200

    Add express & body-parser

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-const sum = (a, b) => a + b
-console.log(sum(10, 5))
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 3000
+
+//middlewares
+app.use(bodyParser.json())
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port} and starting at http://localhost:${port}`)
+})
